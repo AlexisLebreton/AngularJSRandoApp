a43c34e64d44aeccbcd76f4ac5505c9272e745f8
AlexisLebreton <AlexisLebreton@users.noreply.github.com>
2019-05-08 23:21:22 +0200

    list work

--- ClientAppRando/public_html/js/libs/myangular.js/secretaireApp.js
+++ ClientAppRando/public_html/js/libs/myangular.js/secretaireApp.js
@@ -1,10 +1,30 @@
 var randoApp = angular.module('randoApp');
-        
-randoApp.controller('secretaireCtrl', ['$scope',
-    function ($scope) {
-            $scope.testtest = function () {
-                alert("allo");
-            };
-    }
-]);
+
+randoApp.controller('secretaireCtrl', function ($scope, $http) {
+    
+    
+    $http({
+        method: 'GET',
+        url: 'http://localhost:8080/api/randoMembre'})
+            .then(function (response) {
+                $scope.listMembre = response.data;
+                console.log(response.data);
+            });
+/*
+    $scope.majCertifMedical = function (idMembre) {
+        alert(idMembre);
+        $http({method: 'PATCH', url: 'http://localhost:8080/api/randoMembre/majCertifMedical/1'}).
+        then(function(response) {
+          $scope.status = response.status;
+          $scope.data = response.data;
+        }, function(response) {
+          $scope.data = response.data || 'Request failed';
+          $scope.status = response.status;
+      });
+    };
+    */
+
+
+});
+
 
