3a79c1062dacedabc5ae26b0033ea8ff0455aa3d
mahachaouch <maha-chaouch@hotmail.fr>
2019-05-12 18:52:20 +0200

    president

--- ClientAppRando/public_html/js/libs/myangular.js/president.js
+++ ClientAppRando/public_html/js/libs/myangular.js/president.js
@@ -3,5 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var randoApp = angular.module('randoApp');
 
+randoApp.controller('presidentCtrl', function ($scope, $http) {
+
+
+    $http({
+        method: 'GET',
+        url: 'http://localhost:8080/api/randoMembre'})
+            .then(function (response) {
+                $scope.listMembre = response.data;
+                console.log(response.data);
+                $scope.nbMembre = $scope.listMembre.length;
+                $scope.nbMembre = response.data.length;
+                $scope.nbTeamL = 0;
+
+                angular.forEach(response.data, function (value, key) {
+                    angular.forEach(value, function (v1, k1) {//this is nested angular.forEach loop
+                        //console.log(k1+":"+v1);
+
+                        if (k1 == "isTL" && v1) {
+                            $scope.nbTeamL++;
+                        }
+                    });
+                });
+            });
+
+});
 
