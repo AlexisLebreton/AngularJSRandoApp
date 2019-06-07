b6c6c19ecf663eb91c43f6df7ffc54207dfdb63b
chaouch.maha29@gmail.com <maha-chaouch@hotmail.fr>
2019-06-07 09:46:38 +0200

    membre changes

--- ClientAppRando/public_html/js/libs/myangular.js/membreApp.js
+++ ClientAppRando/public_html/js/libs/myangular.js/membreApp.js
@@ -5,10 +5,45 @@
  */
 
 var randoApp = angular.module('randoApp');
-        
+
 randoApp.controller('membreCtrl', function ($scope, $http) {
-    
-    
+
+    $scope.voterDateRando = function (randoId, dateChoisie) {
+        alert(randoId + dateChoisie);
+        var dataToSend = {
+            idMembre: '1',
+            dateChoisie: dateChoisie
+        };
+        
+   /*     $http.post('http://localhost:8181/api/randonnee/voterCreneau/' + randoId, JSON.stringify(data)).then(function (response) {
+
+if (response.data)
+
+$scope.msg = "Post Data Submitted Successfully!";
+
+}, function (response) {
+
+$scope.msg = "Service not Exists";
+
+$scope.statusval = response.status;
+
+$scope.statustext = response.statusText;
+
+$scope.headers = response.headers();
+
+};*/
+
+
+
+        $http({
+            method: 'PATCH',
+            url: 'http://localhost:8181/api/randonnee/voterCreneau/' + randoId})
+                .then(function (response) {
+                    
+                    console.log(response.data);
+                });
+    };
+
     $http({
         method: 'GET',
         url: 'http://localhost:8181/api/randonnee/randoToVotes'})
@@ -16,14 +51,14 @@ randoApp.controller('membreCtrl', function ($scope, $http) {
                 $scope.listRandoVote = response.data;
                 console.log(response.data);
             });
-            
+
     $http({
         method: 'GET',
         url: 'http://localhost:8181/api/randonnee/randoInscriNonCloture'})
             .then(function (response) {
                 $scope.listRandoInscriOuverte = response.data;
                 console.log(response.data);
-    });
+            });
 
 
 });
