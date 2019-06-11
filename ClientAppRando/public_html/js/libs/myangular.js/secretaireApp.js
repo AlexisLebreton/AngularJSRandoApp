463aa4e22cb5b72e64eeeb434a8deafa3bfea723
AlexisLebreton <AlexisLebreton@users.noreply.github.com>
2019-06-11 23:35:09 +0200

    maj certif ok

--- ClientAppRando/public_html/js/libs/myangular.js/secretaireApp.js
+++ ClientAppRando/public_html/js/libs/myangular.js/secretaireApp.js
@@ -10,19 +10,17 @@ randoApp.controller('secretaireCtrl', function ($scope, $http) {
                 $scope.listMembre = response.data;
                 console.log(response.data);
             });
-/*
+
     $scope.majCertifMedical = function (idMembre) {
-        alert(idMembre);
-        $http({method: 'PATCH', url: 'http://localhost:8080/api/randoMembre/majCertifMedical/1'}).
-        then(function(response) {
-          $scope.status = response.status;
-          $scope.data = response.data;
-        }, function(response) {
-          $scope.data = response.data || 'Request failed';
-          $scope.status = response.status;
-      });
+        $http({
+            method: 'PATCH',
+            url: 'http://localhost:8080/api/randoMembre/majCertifMedical',
+            params: {idM: idMembre}})
+                .then(function (response) {
+                    alert("maj certif ok");
+                });
     };
-    */
+    
 
 
 });
