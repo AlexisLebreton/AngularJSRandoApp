7eb15bd45d7a91692657d3c8a0af6f0ac157bdbb
AlexisLebreton <AlexisLebreton@users.noreply.github.com>
2019-06-11 15:43:47 +0200

    teamleader creer rando

--- ClientAppRando/public_html/js/libs/myangular.js/teamleaderApp.js
+++ ClientAppRando/public_html/js/libs/myangular.js/teamleaderApp.js
@@ -6,11 +6,27 @@
  */
 var randoApp = angular.module('randoApp');
 
-randoApp.controller('teamleaderCtrl', function ($scope,$http) {
+randoApp.controller('teamleaderCtrl', function ($scope,$http, $cookies) {
+    
+    var idMember = $cookies.get('coIdM');
+    
+    
                        
-     $scope.creerRando= function() {
-        alert("ok");
-        console.log($scope.titre);
+     $scope.creerRando= function(newRando) {
+        newRando.idTeamLeader = idMember;
+        newRando.date1 =  newRando.date1.getDate().toString().padStart(2, '0') + '-' + (newRando.date1.getMonth() + 1).toString().padStart(2, '0') + '-' +  newRando.date1.getFullYear();
+        newRando.date2 =  newRando.date2.getDate().toString().padStart(2, '0') + '-' + (newRando.date2.getMonth() + 1).toString().padStart(2, '0') + '-' +  newRando.date2.getFullYear();
+        newRando.date3 =  newRando.date3.getDate().toString().padStart(2, '0') + '-' + (newRando.date3.getMonth() + 1).toString().padStart(2, '0') + '-' +  newRando.date3.getFullYear();
+        
+        var postData = angular.toJson(newRando, true);
+        
+        var req = {
+            method: 'POST',
+            url: 'http://localhost:8181/api/randonnee/',
+            data: postData
+           };
+        
+        $http(req).then(function(){ alert("ok"); }, function(){ alert("ko"); });
     };
     
 });
